c3b2f9ec79217c36a960bd0ad6339bc84ecd1d4b
Handi Priyono <handimacbook@gmail.com>
2024-04-07 03:15:22 +0700

    feat: update hooks

--- app/commons/hooks/useNotif.js
+++ app/commons/hooks/useNotif.js
@@ -1,6 +1,4 @@
 import { useEffect, useRef } from "react";
-import { Platform } from "react-native";
-import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
 
 export default function useNotifHook() {
@@ -8,8 +6,6 @@ export default function useNotifHook() {
   const responseListener = useRef();
 
   useEffect(() => {
-    registerForPushNotificationsAsync().then((token) => {});
-
     notificationListener.current =
       Notifications.addNotificationReceivedListener((notification) => {});
 
@@ -32,43 +28,4 @@ export async function sendNotif(content) {
     content: content,
     trigger: null,
   });
-}
-
-async function registerForPushNotificationsAsync() {
-  let token;
-
-  if (Platform.OS === "android") {
-    await Notifications.setNotificationChannelAsync("default", {
-      name: "default",
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: "#FF231F7C",
-    });
-  }
-
-  if (Device.isDevice) {
-    const { status: existingStatus } =
-      await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== "granted") {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== "granted") {
-      alert("Failed to get push token for push notification!");
-      return;
-    }
-    // Learn more about projectId:
-    // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
-    token = (
-      await Notifications.getExpoPushTokenAsync({
-        projectId: "your-project-id",
-      })
-    ).data;
-    // console.log(token);
-  } else {
-    alert("Must use physical device for Push Notifications");
-  }
-
-  return token;
 }
