21461b44df67ebd5727d5391e6e29b2979e99434
Handi Priyono <handimacbook@gmail.com>
2024-04-07 05:59:53 +0700

    feat: add empty state fav.

--- app/screens/Favourite/index.js
+++ app/screens/Favourite/index.js
@@ -1,6 +1,8 @@
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, Image, View, Text } from "react-native";
 import SongList from "../../commons/component/SongList/index";
 import useMusicStore from "../../commons/store/index";
+const ImgFav = require("../../../assets/favourite.png");
+import Styles from "./style";
 
 const Favourite = () => {
   const favList = useMusicStore((state) => state.favoriteList);
@@ -8,7 +10,19 @@ const Favourite = () => {
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
-      <SongList data={dataFav} />
+      {dataFav?.length ? (
+        <SongList data={dataFav} />
+      ) : (
+        <View style={Styles.notFound}>
+          <Image style={Styles.img} source={ImgFav} />
+          <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+            No Favorites Yet!
+          </Text>
+          <Text style={{ fontSize: 14, paddingTop: 10, textAlign: "center" }}>
+            Tap the heart icon to add songs to your collection.
+          </Text>
+        </View>
+      )}
     </SafeAreaView>
   );
 };
---
+++ app/screens/Favourite/style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  notFound: {
+    flex: 1,
+    padding: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 22,
+  },
+  img: { width: 150, height: 180 },
+});
