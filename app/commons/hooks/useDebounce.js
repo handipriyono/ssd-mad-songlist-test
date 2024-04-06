28d7513d7f55ff4c31f4d00b5319fa47fc99485b
Handi Priyono <handimacbook@gmail.com>
2024-04-07 00:25:16 +0700

    feat: add hooks for shared

---
+++ app/commons/hooks/useDebounce.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+export default function useDebounceValue(value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value]);
+
+  return debouncedValue;
+}
